214bd73e06264f5edae6e3d63c73ee68c1828641
Jeremy Yochum <jyochum391@yahoo.com>
2023-07-30 12:43:59 -0400

    pulled and displayed default page information

--- js/app.js
+++ js/app.js
@@ -12,11 +12,18 @@ const main = document.getElementById('main');
 
 function createEmployee(data) {
 
-    console.log(data[0].picture.large);
+    console.log(data[0]);
     for (let i = 0; i < 13; i++) {
         const section = document.createElement('section');
+        const fName = data[i].name.first;
+        const lName = data[i].name.last;
+        const email = data[i].email;
+        const city = data[i].location.city;
         const html = `
             <img src="${data[i].picture.large}">
+            <h3>${fName} ${lName}</h3>
+            <p>${email}</p>
+            <p>${city}</p>
         `;
         section.innerHTML = html;
         section.className = 'card';
@@ -31,8 +38,9 @@ function createEmployee(data) {
 Promise.all([
     fetch('https://randomuser.me/api/?results=12')
         .then(res => res.json())
-        // .then(res => console.log(res.results))
         .then(res => createEmployee(res.results))
+        .then(r => console.log(r.results))
+        .catch(err => `There was an error loading data: ${err}`)
 ])
 
 
