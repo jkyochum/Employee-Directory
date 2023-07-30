6af01001dfe4ab806a511e965601554bea086ff5
Jeremy Yochum <jyochum391@yahoo.com>
2023-07-30 02:49:38 -0400

    working on converting fetched data

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 
-function fetchData(url) {
-    return fetch(url)
-        .then(result => result.json())
-        .catch(err => console.log(`There was a problem loading the data: ${err}`))
+// function fetchData(url) {
+//     return fetch(url)
+//         .then(result => result.json())
+//         .catch(err => console.log(`There was a problem loading the data: ${err}`))
 
-}
+// }
 
-fetchData('https://randomuser.me/api/?results=12')
-    .then(data => console.log(data))
+// function checkStatus(res) {
+//     if (res === 'ok')
+// }
 
+
+// function createEmployee() {
+//     const employeeList = fetchData('https://randomuser.me/api/?results=12');
+
+// }
+
+
+// fetchData('https://randomuser.me/api/?results=12')
+//     .then(data => console.log(data))
+Promise.all([
+    fetch('https://randomuser.me/api/?results=12')
+        .then(res => res.json())
+        .then(res => document.getElementByTagName('main').innerHTML = res)
+])
 
 
 //image, first and last name, email, city
