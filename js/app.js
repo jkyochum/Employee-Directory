7ce096dcf73bb5a5f01bd7b9cacffce3da9e9273
Jeremy Yochum <jyochum391@yahoo.com>
2023-08-03 14:12:57 -0400

    abbreviate state names in modal

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const employeeArray = [];
 
 
 //Using fetch API to request data from randomuser.me
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?nat=us&results=12')
     .then(res => res.json())
     .then(data => {
         console.log(data);
@@ -60,10 +60,73 @@ function createEmployeeModal(data) {
         <p>${city}</p>
         <div id="border"></div>
         <p>${cell}</p>
-        <p>${streetNum} ${streetName} ${city}, ${state} ${postCode}</p>
+        <p>${streetNum} ${streetName} ${city}, ${abbreviateState(state)} ${postCode}</p>
         <p>Birthday: ${birthDate}</p>
     `;
     modalCard.innerHTML = html;
+}
+
+//HELPER FUNCTIONS
+function abbreviateState(state) {
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    for (let i = 0; i < states.length; i++) {
+        if (states[i][0] === state) {
+            return states[i][1];
+        }
+    }
+    return 'Unavailable';
 }
 
 
